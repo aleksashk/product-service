c8f32576f3c6a1250858658590100fdab9ebcf2b
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2024-08-06 12:14:41 +0300

    refactoring ProductController

--- src/main/java/by/flamexander/microservices/product/controller/ProductController.java
+++ src/main/java/by/flamexander/microservices/product/controller/ProductController.java
@@ -1,16 +1,21 @@
 package by.flamexander.microservices.product.controller;
 
 import by.flamexander.microservices.product.dto.ProductRequest;
+import by.flamexander.microservices.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
-
+        productService.createProduct(productRequest);
     }
 }
